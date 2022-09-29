dd71c098f7d2ae585a8cc55f52b51a05bb5d926d
EdvardJohansen <edvarddallj97@gmail.com>
2022-09-29 12:27:44 +0200

    2nd Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">My Fullstack Project</div>;
+  return (
+    <div className="App">
+      <div>
+        <h1>My Fullstack Project</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
